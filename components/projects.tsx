26bf38a20933733562fc5bc409398eebb5daedf9
AfamOkoh <afamokoh@gmail.com>
2022-12-09 23:51:28 +0000

    style: remove card changing to the side on larger screens

--- components/projects.tsx
+++ components/projects.tsx
@@ -9,7 +9,7 @@ interface Props {}
  *
  * @todo  implement a simple responsive projects page using cards and grid.
  * @todo I will start with 2 cards that go in a grid that responsively changes to large cards when resized
- *
+ * @todo Set p tag in both projects to have max widths when larger to avoid images being squashed.
  */
 const Projects: NextPage<Props> = () => {
   return (
@@ -17,22 +17,40 @@ const Projects: NextPage<Props> = () => {
       className='w-screen border-4 border-indigo-600 max-h-fit lg:pt-16 bg-base-200'
       id='projects'>
       <h1 className='py-10 pl-2'>Projects</h1>
-      <div className='shadow-xl card lg:card-side bg-base-100'>
+      <div className='shadow-xl card bg-base-100'>
         <figure>
           <Image
             src={ProjectRedeemImage}
             alt="image of a project I've worked on"
-            className='w-full'
+            className='w-full lg:w-10/12'
           />
         </figure>
         <div className=' h-fit card-body'>
-          <h2 className=''>Project Redeem!</h2>
+          <h3 className=''>Project Redeem!</h3>
           <p>
             Including a four week full stack project that allowed people to
             share tutorials for building home grown technology.
           </p>
 
-          <button className='btn btn-primary'>See it in Action</button>
+          <button className='btn btn-primary w-36'>See it in Action</button>
+        </div>
+      </div>{' '}
+      <div className='my-1 shadow-xl card bg-base-100'>
+        <figure>
+          <Image
+            src={ProjectSorcery}
+            alt="image of a project I've worked on"
+            className='w-full lg:w-10/12'
+          />
+        </figure>
+        <div className=' h-fit card-body'>
+          <h3 className=''>Project Sourcery!</h3>
+          <p>
+            Sourcery is a centralized platform for posting and viewing resources
+            that could be of use to School of Code bootcampers.
+          </p>
+
+          <button className='btn btn-primary w-36'>See it in Action</button>
         </div>
       </div>{' '}
     </div>
@@ -124,10 +142,10 @@ Previous Sourcery Code
   <div id="item4" className="w-full carousel-item">
     <img src="https://placeimg.com/800/200/arch" className="w-full" />
   </div>
-</div> 
-<div className="flex justify-center w-full gap-2 py-2">
+  </div> 
+  <div className="flex justify-center w-full gap-2 py-2">
   <a href="#item1" className="btn btn-xs">1</a> 
   <a href="#item2" className="btn btn-xs">2</a> 
   <a href="#item3" className="btn btn-xs">3</a> 
   <a href="#item4" className="btn btn-xs">4</a>
-</div> */
+  </div> */
