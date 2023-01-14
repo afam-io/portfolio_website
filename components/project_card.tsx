7bcbfa4b3be6531dd06bf5608522fde5bb274f68
AfamOkoh <afamokoh@gmail.com>
2023-01-14 23:09:12 +0000

    refactor: start on project card refactor

--- components/project_card.tsx
+++ components/project_card.tsx
@@ -1,38 +1,97 @@
 import React, { ReactComponentElement } from 'react';
-import Image from 'next/image';
+import Image, { StaticImageData } from 'next/image';
+import Link from 'next/link';
+import { UrlObject } from 'url';
 interface Props {
-  projects: {
-    imgSrc: string;
+ 
+    imgSrc: StaticImageData;
     imgAlt: string;
     title: string;
     projectDescription: string;
-    projectLink: string;
-  };
+    repoLink: string;
+    deploymentLink?: string;
+ 
 }
 
 // const Projects: NextPage<Props> = () => {
 
-const ProjectCard = ({ projects }: Props) => {
+const ProjectCard = ({  imgSrc,
+  imgAlt,
+  title,
+  projectDescription,
+  repoLink, deploymentLink }: Props) => {
   return (
-    <div className='shadow-xl card lg:card-side bg-base-100'>
-      <figure>
-        <Image
-          src='/../public/images/afam_headshot_books_cropped.jpg'
-          alt="image of a project I've worked on"
-          className='w-full'
-          height={300}
-          width={300}
-        />
-      </figure>
-      <div className='card-body'>
-        <h2 className='card-title'>New album is released!</h2>
-        <p>Click the button to listen on Spotiwhy app.</p>
-        <div className='justify-end card-actions'>
-          <button className='btn btn-primary'>Listen</button>
+    <section className='shadow-xl card bg-base-100'>
+        <figure>
+          <Image
+            src={imgSrc}
+            alt={imgAlt}
+            className='w-full '
+          />
+          {/* <figcaption>
+            Image of project Redeems homepage showing eco-friendly tutorials
+          </figcaption> */}
+        </figure>
+        <div className=' h-fit p-[0.6rem] md:py-[1.25rem] md:pl-[1rem] card-body'>
+          <h3 className=''>{title}</h3>
+          <p>
+            {projectDescription}
+          </p>
+
+          <Link
+            href={
+              repoLink
+            }
+            target='_blank'>
+            <button className='btn btn-primary w-36'>GitHub Repository</button>
+          </Link>
+
+          <Link
+            href={deploymentLink}
+            target='_blank'>
+            <button className='btn btn-primary w-36'>Site Deployment</button>
+          </Link>
         </div>
-      </div>
-    </div>
+      </section>{' '}
   );
 };
 
 export default ProjectCard;
+
+/* 
+
+ <section className='shadow-xl card bg-base-100'>
+        <figure>
+          <Image
+            src={ProjectRedeemImage}
+            alt="image of a project I've worked on"
+            className='w-full '
+          />
+          {/* <figcaption>
+            Image of project Redeems homepage showing eco-friendly tutorials
+          </figcaption> */}
+       /*   </figure>
+          <div className=' h-fit p-[0.6rem] md:py-[1.25rem] md:pl-[1rem] card-body'>
+            <h3 className=''>Project Redeem!</h3>
+            <p>
+              Including a four week full stack project that allowed people to
+              share tutorials for building home grown technology.
+            </p>
+  
+            <Link
+              href={
+                'https://github.com/afam-io/final-project-front-end-boolean-pirates'
+              }
+              target='_blank'>
+              <button className='btn btn-primary w-36'>GitHub Repository</button>
+            </Link>
+  
+            <Link
+              href={'https://final-project-front-end-boolean-pirates.vercel.app/'}
+              target='_blank'>
+              <button className='btn btn-primary w-36'>Site Deployment</button>
+            </Link>
+          </div>
+        </section>{' '}
+
+*/
